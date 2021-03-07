0de92fc9e74e514443e5ae8e73e5587701dc768c
5krc1 <skrjancmatic14@gmail.com>
2021-03-07 15:20:54 +0100

    h

---
+++ app.js
@@ -0,0 +1,22 @@
+let card = document.querySelectorAll('.character');
+
+card.forEach((e) => {
+    e.addEventListener('click', () => {
+        if(e.classList.value != 'character'){
+            e.classList.toggle('active');
+            e.classList.add('character');
+        }
+        else{
+            removeActiveClasses();
+            e.classList.toggle('active');
+            e.classList.remove('character')
+        }
+    });
+})
+
+function removeActiveClasses(){
+    card.forEach(e => {
+        e.classList.remove('active');
+        e.classList.add('character');
+    })
+}
