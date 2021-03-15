492d1121d316886b02d060b07b3e8552f850ff1d
5krc1 <skrjancmatic14@gmail.com>
2021-03-15 16:03:03 +0100

    u

--- website/views.py
+++ website/views.py
@@ -5,3 +5,28 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template('index.html')
+
+@views.route('/archer')
+def archer():
+    return render_template('archer.html')
+
+@views.route('/knight')
+def knight():
+    return render_template('knight.html')
+
+@views.route('/butcher')
+def butcher():
+    return render_template('butcher.html')
+
+@views.route('/ghost')
+def ghost():
+    return render_template('ghost.html')
+
+@views.route('/chef')
+def chef():
+    return render_template('chef.html')
+
+@views.route('/ninja')
+def ninja():
+    return render_template('ninja.html')
+
